507e7931bfe4ce2ba957998db0109a6014191148
errorcode-4o4 <avinashk.ug20.ee@nitp.ac.in>
2022-04-11 15:32:52 +0530

    commit on 'master'

---
+++ question03.java
@@ -0,0 +1,8 @@
+public class question03 {
+    public static void main(String[] args) {
+        long c = 1010101;
+        Long obj4 = Long.valueOf(c);
+        System.out.println("Initial value : " + c);
+        System.out.println("After Conversion : " + obj4);
+    }
+}
